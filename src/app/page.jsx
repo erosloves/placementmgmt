5ac5c18d00edde65059412eb35a2b7641bf53d7d
3ricwebdev <ericxdd@yandex.ru>
2024-10-09 00:20:03 +0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,81 @@
+"use client";
+import { animate, AnimatePresence, motion } from "framer-motion";
 import css from "./page.module.css";
+import { useRef, useState } from "react";
+
 export default function Page() {
+  const [isPopUpActive, setPopUpActive] = useState(false);
+  const animation = {
+    init: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+  };
   return (
     <div className={css.home} style={{ fontSize: 0 }}>
       <section className={css.promo}>
-        <span className={css.ticker}>
-          <span className={css.ticker_item}>
-            models | fashion | agencies | beauty | creators | networking |
-            education | photographers | knowleges | blogging | people | models |
-            fashion | agencies | beauty | creators | networking | education |
-            photographers | knowleges | blogging | people |
-          </span>
-          <span className={css.ticker_item}>
-            models | fashion | agencies | beauty | creators | networking |
-            education | photographers | knowleges | blogging | people | models |
-            fashion | agencies | beauty | creators | networking | education |
-            photographers | knowleges | blogging | people |
-          </span>
-          <span className={css.ticker_item}>
-            models | fashion | agencies | beauty | creators | networking |
-            education | photographers | knowleges | blogging | people | models |
-            fashion | agencies | beauty | creators | networking | education |
-            photographers | knowleges | blogging | people |
-          </span>
-          <span className={css.ticker_item}>
-            models | fashion | agencies | beauty | creators | networking |
-            education | photographers | knowleges | blogging | people | models |
-            fashion | agencies | beauty | creators | networking | education |
-            photographers | knowleges | blogging | people |
-          </span>
-        </span>
-        <div className={css.layout}></div>
-        <video autoPlay playsInline muted loop>
-          <source src="/header.mp4" />
-        </video>
-        <span className={css.ticker}>
-          models | fashion | agencies | beauty | creators | networking |
-          education | photographers | knowleges | blogging | people | models |
-          fashion | agencies | beauty | creators | networking | education |
-          photographers | knowleges | blogging | people |
-        </span>
+        <AnimatePresence mode="wait">
+          {!isPopUpActive && (
+            <motion.div
+              className={css.promo_item}
+              onClick={() => setPopUpActive(true)}
+              variants={animation}
+              initial="init"
+              animate="animate"
+              exit="init"
+            >
+              Get scouted | Стать моделью
+            </motion.div>
+          )}
+          {isPopUpActive && (
+            <PopUp
+              isPopUpActive={isPopUpActive}
+              setPopUpActive={setPopUpActive}
+              variants={animation}
+            />
+          )}
+        </AnimatePresence>
       </section>
     </div>
   );
 }
+
+const PopUp = ({ setPopUpActive, variants }) => {
+  return (
+    <motion.div
+      className={css.popup}
+      variants={variants}
+      initial="init"
+      animate="animate"
+      exit="init"
+    >
+      <form>
+        <div className={css.close} onClick={() => setPopUpActive(false)}>
+          ✕
+        </div>
+        <input className={css.data} placeholder="Name" type="text"></input>
+        <input className={css.data} placeholder="Age" type="number"></input>
+        <input className={css.data} placeholder="Height" type="number"></input>
+        <input className={css.data} placeholder="Email" type="email"></input>
+        <input className={css.data} placeholder="Instagram" type="text"></input>
+      </form>
+      <button className={css.add}>Add files</button>
+      <div className={css.popup_item}>
+        <label className={css.checkbox}>
+          <input type="checkbox"></input>I agree to the processing of my
+          personal data in accordance with the privacy policy and terms of use
+          of the website.
+        </label>
+        <button>Submit</button>
+      </div>
+      <div className={css.img_container}>
+        <img src="/faces/artemii/IMG_6314.PNG" alt="" />
+        <img src="/faces/eldar/IMG_6348.JPEG" alt="" />
+        <img src="/faces/muza/muza.png" alt="" />
+        <img src="/faces/diana_zvereva/IMG_6457.JPG" alt="" />
+      </div>
+    </motion.div>
+  );
+};
